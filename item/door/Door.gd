5b0299d43032603aec41f5331e2b61cb92e45ac8
Your Name <you@example.com>
2020-09-26 21:11:26 +0000

    fire2

--- item/door/Door.gd
+++ item/door/Door.gd
@@ -4,10 +4,10 @@ onready var globals = get_node("/root/Globals")
 
 func _on_Door_body_entered(body):
 	if body.is_in_group("player") and globals.player_has_class:
-		
+		get_tree().change_scene("res://level/Level"+str(globals.level+1)+".tscn")
 		globals.level += 1
 		globals.player_has_class = false
 		globals.hasJetpack = false
 		globals.jetpackIsActive = false
 		globals.hasGun = false
-		return get_tree().change_scene("res://level/Level"+str(globals.level+1)+".tscn")
+	return
